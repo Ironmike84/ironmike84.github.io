cc6ddab21b09b0d1d1d78de25f84d9d60b02a59e
Mike <87212248+Ironmike84@users.noreply.github.com>
2021-12-14 07:04:36 -0500

    Update

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-// module.exports = require('./lib/express');
+
 const express = require('express')
 const app = express()
 
 
-// app.use('C:/Users/TRDFRG/School/Movie_API/Documentation.html', express.static('public'));
-
-
-
-
 app.get('/', function (req, res) {
   app.use('/documentation.html', express.static(__dirname + '/documentation.html'));
-  res.send('Hello World')
+  res.send('This is the Documentation Page.')
 })
 
-app.get('/Movies', (req, res) => {
+app.get('/movies', (req, res) => {
   let responseText = 'This will get your movies!.';
   responseText += '<small>Requested at: ' + req.requestTime + '</small>';
   res.send(responseText);
