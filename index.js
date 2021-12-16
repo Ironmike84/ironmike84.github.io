e44959cada48bd0204fca87ede854ce6c7136501
Mike <87212248+Ironmike84@users.noreply.github.com>
2021-12-16 18:28:34 -0500

    Update

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const { default: regex } = require('uuid/dist/regex');
 const app = express()
-const PORT = 3000
+const PORT = 8080
 
 app.use(express.static('public'));
 
 app.get('/', (req, res)=>{
+  res.send('/public/documentation.html')
     res.send('HELLO MIKE!!!')
 })
 
