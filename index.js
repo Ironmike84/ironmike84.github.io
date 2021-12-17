40a7ad3df507beb5b433b536386b221dbd8f8dd6
Mike <87212248+Ironmike84@users.noreply.github.com>
2021-12-16 21:15:28 -0500

    Update

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const path = require('path');
+morgan = require('morgan');
+
+app.use(morgan('public'));
 
 app.use(express.static('public'));
 
-app.use('/documentation', express.static('public'));
+app.use('/public', express.static('public'));
 
 app.get('/', (req, res)=>{
-    res.send('This Didnt work!!')
+    res.send('This Works!!!!')
 })
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
+
+
 
 app.listen(PORT, ()=>(console.log(`Listening On Port: ${PORT}`)))
