66d672e54e6453fab66840b5eaebdb8a6f335490
Mike <87212248+Ironmike84@users.noreply.github.com>
2022-01-06 12:47:54 -0500

    Add files via upload

---
+++ Insert.js
@@ -0,0 +1,11 @@
+var  me = {
+    _id: (3006),
+    Username: "JamesSmart",
+    Password: "iamsmart",
+    Email: "JamesSmart@gmail.com",
+    Birthday: "92-04-07",
+  FavoriteMovies:[{_id: (2002), Title: 'Iron Man', Genre: 'Action'},
+  {_id: (2004), Title: 'Braveheart', Genre: 'Drama'},
+  {_id: (2008), Title: 'SpiderMan', Genre: 'Action'}],
+  ImagePath: "JamesSmart"
+}
