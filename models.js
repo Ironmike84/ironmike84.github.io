797ddc8eb61d3cf9100d22000457748119fb2c0e
Mike <87212248+Ironmike84@users.noreply.github.com>
2022-01-09 10:14:07 -0500

    Add files via upload

--- models.js
+++ models.js
@@ -1,5 +1,11 @@
+
+//============================================// MODELS  \\============================================================
+
+//------------------------------------------------------------------------------// IMPORTS Require
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/MuVies', { autoIndex: false });
+
+//------------------------------------------------------------------------------// MOVIE MODEL SCHEMA
 let movieSchema = mongoose.Schema({
         _id: {type: Number, required: false},
         Title: String,
@@ -21,8 +27,7 @@ let movieSchema = mongoose.Schema({
         ImagePath: String,
         Featured: Boolean
 });
-
-
+//------------------------------------------------------------------------------// USER MODEL SCHEMA
 let usersSchema = mongoose.Schema({
     _id: {type: Object},
       UserName: String,
@@ -33,9 +38,7 @@ let usersSchema = mongoose.Schema({
 
     ImagePath: String
 });
-
-
-
+//------------------------------------------------------------------------------// DIRECTOR MODEL SCHEMA
 let directorSchema = mongoose.Schema({
   _id: {type: Object},
     Name: String,
@@ -46,27 +49,28 @@ let directorSchema = mongoose.Schema({
   Featured: Boolean,
   Movies: [{type: Object}]
 });
-
-
+//------------------------------------------------------------------------------// GENRE MODEL SCHEMA
 let genreSchema = mongoose.Schema({
   _id: {type: Object},
   Name: String,
   Description: String
 });
-
+//------------------------------------------------------------------------------// FAVMOVIES MODEL SCHEMA
 let favMoviesSchema = mongoose.Schema({
   _id: {type:Object},
   UserName: String,
   FavoriteMovies: [{_id:Object}]
 })
 
-
+//------------------------------------------------------------------------------// MODELS Variables
   let Movie = mongoose.model('Movie', movieSchema);
   let users = mongoose.model('users', usersSchema);
   let Directors = mongoose.model('Directors', directorSchema);
   let genres = mongoose.model('genres', genreSchema);
   let favMovies = mongoose.model('favMovies', favMoviesSchema);
 
+
+//------------------------------------------------------------------------------// EXPORT Models
   module.exports.Movie = Movie;
   module.exports.users = users;
   module.exports.Directors = Directors;
