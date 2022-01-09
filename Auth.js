b8b12c0f1d30e50855df0807cbadaa32c6b6bbc9
Mike <87212248+Ironmike84@users.noreply.github.com>
2022-01-08 19:16:20 -0500

    Authorization

--- Auth.js
+++ Auth.js
@@ -17,7 +17,7 @@ let generateJWTToken = (user) => {
 
 /* POST login. */
 module.exports = (router) => {
-  router.post('/login', (req, res) => {
+  router.post('/Login', (req, res) => {
     passport.authenticate('local', { session: false }, (error, user, info) => {
       if (error || !user) {
         return res.status(400).json({
